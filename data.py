8e478a22e2b74325924bb7c476e894aab9f89162
Garetelan <mmerbach1@web.de>
2018-11-03 00:44:37 +0100

    Added Methods to Handle the Data from the CSV-File

--- data.py
+++ data.py
@@ -1,28 +1,78 @@
 
-
-
+import datetime
+from dateutil.parser import parse
+import random
 class Members():
+    self.fileObject
+    self.dataArray
+    self.dataDictionary = {}
+
     def __init__(self):
         #read file and store to internal mem
+        self.loadDataFromFile(pathAndFileName)
 
 
-    def proofMember(userkey):
+    def proofMember(self,userkey):
         #should return name, forename, lastseen
         # as tuple
         #empty tuple if user is not in list
+        if(self.dataDictionary.has_key(userkey))
+        return dataDictionary[userkey]
+        else
+        return{}
 
-
-    def addMember(name, forname):
-        #gen key
+    def addMember(self, id, name, forname):
+        this.generateUserKey(id,name,forname)
+        this.persist()
         #add to internal
         #persist
 
 
+    def generateUserKey(self,id,name, forename):
+        mMember = Member.__init__(id,name,forname)
+        sKey = mMember.getKey()
+        dataDictionary[key] = mMember.addMemberToDictionary()
+        #Generate random Key, add Data given from GUI, save into file...?... Profit
 
 
-    def generateUserKey(name, forename):
-        genStuff
-
-    def persist():
+    def persist(self):
         #save internal storage
         #should be invoked by addmember
+        self.fileObject.open()
+            with iIndex in self.dataDictionary
+            self.fileObject.write(self.dataDictionary.values())
+
+        self.fileObject.open()
+
+    def loadDataFromFile(self,pathAndFileName):
+        #Load data from File the file should be in the same directory
+        with open(pathAndFileName,"r+") as fileObject
+        dataArray = fileObject.readlines()
+         for iIndex in dataArray
+            dataLine = dataArray[iIndex]
+            key = dataLine[5]
+            value = dataLine
+            dataDictionary[key] = value
+
+
+class Member():
+    def __init__(self, id, sMemberName,sMemberFirstName):
+        self.id = id
+        self.name = sMemberName
+        self.firstname = sMemberFirstName
+        self.datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.key =  random.randint(1,4294967295)
+
+    def addMemberToDictionary(self):
+        return arrayData = [self.id,self.name,self.firstname,self.datetime,self.key]
+
+    def getKey(self):
+            return self.key
+    def getName(self):
+            return self.name
+    def getForName(self)
+            return self.firstname
+    def getlastSeen(self)
+        return self.datetime
+    def getId(self)
+        return self.id
