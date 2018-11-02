d66642a1220525afb0176146ba842055de39b972
legion <legion@legions>
2018-11-02 20:26:53 +0100

    data stub

---
+++ data.py
@@ -0,0 +1,28 @@
+
+
+
+class Members():
+    def __init__(self):
+        #read file and store to internal mem
+
+
+    def proofMember(userkey):
+        #should return name, forename, lastseen
+        # as tuple
+        #empty tuple if user is not in list
+
+
+    def addMember(name, forname):
+        #gen key
+        #add to internal
+        #persist
+
+
+
+
+    def generateUserKey(name, forename):
+        genStuff
+
+    def persist():
+        #save internal storage
+        #should be invoked by addmember
